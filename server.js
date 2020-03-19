ee4fd6f5847ed68d602ca869e75bc6c733ed2fa0
Aditi R <aditi.seattle@gmail.com>
2020-03-18 18:22:41 -0700

    Added profile and posts APIs

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const expressApp = express();
 
 expressApp.use('/api/users', users);
+expressApp.use('/api/profile', profile);
+expressApp.use('/api/posts', posts);
 
 //Configure port to run web app
 const port=8080;
